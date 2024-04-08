0abc8c76234cbefc818847bf00e9f79a144873c6
fareedjaved203 <fareedjaved203@gmail.com>
2024-04-08 18:04:51 +0500

    order functionality added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import userRouter from "./routes/user.routes.js";
+import orderRouter from "./routes/orders.routes.js";
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/orders", orderRouter);
 
 export { app };
